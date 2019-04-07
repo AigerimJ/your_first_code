72077f8f56c36a4165b1e79fefcfc392a839b6ef
Aigerim Janaliyeva <aigerim.janaliyeva@nu.edu.kz>
2019-04-07 19:19:19 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
-int main() {
-  for (int i = 0; i < 32; i++) {
-    printf(" Hello Holberton\n");
+int main() 
+{
+  int i;
+  for (i = 0; i < 32; i++) 
+  {
+    printf("Hello Holberton\n");
   }
   return 0;
 }
